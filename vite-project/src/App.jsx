8fa76f66e21884dbccb66116b5aecfdc52b8992c
chc216 <mine3386@naver.com>
2023-11-10 02:34:23 +0900

    mainpage routing 설정

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,16 @@
 import { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import MainPage from './page/mainPage.jsx';
 
 function App() {
-
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/mainpage" element={<MainPage />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App
