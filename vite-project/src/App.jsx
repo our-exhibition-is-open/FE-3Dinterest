3de15b14ad1cc9e4d5ddca7ec1e7ee052bd7ec9d
chc216 <mine3386@naver.com>
2023-11-10 02:45:01 +0900

    각 페이지별 라우팅 설정

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -2,12 +2,18 @@ import { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import MainPage from './page/mainPage.jsx';
+import LoginPage from './page/LoginPage.jsx';
+import UploadPage from './page/UploadPage.jsx';
+import ContributePage from './page/ContributePage.jsx';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/mainpage" element={<MainPage />} />
+                <Route path="/Loginpage" element={<LoginPage />} />
+                <Route path="/Uploadpage" element={<UploadPage />} />
+                <Route path="/Contributepage" element={<ContributePage />} />
             </Routes>
         </BrowserRouter>
     );
