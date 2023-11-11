b8d2b86d39b80e473e8019f3c557f2a2a51ed94c
chc216 <mine3386@naver.com>
2023-11-11 18:08:48 +0900

    자잘한 경로 수정사항 및 threejs패키지 설치

---
+++ vite-project/src/component/mainpage/MasonryList.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from 'styled-components';
+
+export default function MasonryList() {
+    return 
+}
--- vite-project/src/page/mainPage.jsx
+++ vite-project/src/page/mainPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { NavigationBar } from '../component/common/NavigationBar.jsx'
+import { NavigationBar } from '../component/common/NavigationBar.jsx';
+import { PostCard } from '../component/common/PostCard.jsx';
+
+
 
 export default function MainPage() {
     return(
         <>
             <NavigationBar />
+            <PostCard />
         </>
     );
 }
