c12ae8a5f6f1741a53a3fd052dc0cc32b1addf65
chc216 <mine3386@naver.com>
2023-11-12 20:46:09 +0900

    코드 컨벤션에 맞게 이쁘게 리팩토링

---
+++ vite-project/src/component/common/ButtonComponent.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { styled } from 'styled-components';
+
+
+const Button = styled.h1`
+    cursor: pointer;
+
+    width: 70px;
+    height: 28px;
+    display: inline-block; 
+    position: relative;
+
+    text-decoration: none;
+    color: #000;
+    text-align: center;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+
+    font-family: "SF-Pro-Rounded-Regular";
+
+    &:after {
+        content: "";
+        display: block;
+        width: 100%; height: 2px;
+        background: #000;
+        transition: all 0.1s;
+    }
+    &:after {
+        transform: scaleX(0);
+    }
+    &:hover:after {
+        transform: scaleX(0.8);
+    }
+`;
+
+export function ButtonComponent(props) {
+    const navigate = useNavigate();
+    const buttonName = props.buttonName;
+    const route = props.route;
+    
+    function handleClick() {
+        navigate(route);
+    }
+
+    return(
+        <Button onClick={handleClick}>
+            {buttonName}
+        </Button>
+    );
+}
--- vite-project/src/component/common/NavigationBar.jsx
+++ vite-project/src/component/common/NavigationBar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+
 import { styled } from 'styled-components';
+import { ButtonComponent } from "./ButtonComponent";
 
 const Header = styled.header`
         display: flex;
@@ -13,57 +13,7 @@ const Header = styled.header`
     
         background-color: #ffffff;
         box-shadow: 0px 0px 10px 0.1px rgba(0, 0, 0, 0.25);
-    `;
-
-const Button = styled.h1`
-    cursor: pointer;
-
-    width: 70px;
-    height: 28px;
-    display: inline-block; 
-    position: relative;
-
-    text-decoration: none;
-    color: #000;
-    text-align: center;
-    font-size: 15px;
-    font-style: normal;
-    font-weight: 600;
-    line-height: normal;
-
-    font-family: "SF-Pro-Rounded-Regular";
-
-    &:after {
-        content: "";
-        display: block;
-        width: 100%; height: 2px;
-        background: #000;
-        transition: all 0.1s;
-    }
-    &:after {
-        transform: scaleX(0);
-    }
-    &:hover:after {
-        transform: scaleX(0.8);
-    }
-
-`
-
-function ButtonComponent(props) {
-    const navigate = useNavigate();
-    const buttonName = props.buttonName;
-    const route = props.route;
-    
-    function handleClick() {
-        navigate(route);
-    }
-
-    return(
-        <Button onClick={handleClick}>
-            {buttonName}
-        </Button>
-    );
-}
+`;
 
 export function NavigationBar() {
     return (
