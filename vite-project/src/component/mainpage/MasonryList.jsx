5029098cddd612fb9a6b3480ccf85d21d9d50e5e
chc216 <mine3386@naver.com>
2023-11-12 20:28:28 +0900

    추후 유지보수성을 위한 컴포넌트 분리

--- vite-project/src/component/mainpage/MasonryList.jsx
+++ vite-project/src/component/mainpage/MasonryList.jsx
@@ -2,7 +2,25 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from 'styled-components';
+import { PostCard } from "../common/PostCard";
 
-export default function MasonryList() {
-    return 
+const Container = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    margin-left: 48px;
+    margin-right: 48px;
+    margin-top: 20px;
+    gap: 15px;
+`
+
+export function MasonryList(props) {
+    const postList = props.dataList.map(data => 
+        <PostCard imgUrl={data.imgUrl} title={data.title} key={data.postId} />
+        );
+
+    return (
+        <Container>
+            {postList}
+        </Container>
+    );
 }
