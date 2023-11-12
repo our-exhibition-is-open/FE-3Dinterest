bbda9a4bb7d69cb8cf02e5d6e8b51f35785d56d1
chc216 <mine3386@naver.com>
2023-11-12 20:27:58 +0900

    게시글 컴포넌트 리팩토링

--- vite-project/src/component/common/PostCard.jsx
+++ vite-project/src/component/common/PostCard.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { styled } from 'styled-components';
+import * as THREE from 'three';
+
 
 const Container = styled.div`
     display: flex;
@@ -11,12 +13,19 @@ const Container = styled.div`
     gap: 11px;
 `
 const PostWrapper = styled.div`
+    display: flex;
+    flex-direction: column;
     width: 210px;
     height: 373px;
-    color: Black;
     border-radius: 0.9375rem;
+    overflow: hidden;
+`
 
-    background-image: url('src/resource/image/1024025.jpeg');
+const ImageWrapper = styled.img`
+    border-radius: 0.9375rem;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `
 
 const TitleWrapper = styled.div`
@@ -26,21 +35,23 @@ const TitleWrapper = styled.div`
 
     color: Black;
     font-family: "SF-Pro-Rounded-Regular";
-    font-size: 0.814rem;
+    font-size: 0.84rem;
     font-style: normal;
     font-weight: 600;
     line-height: normal;
     letter-spacing: 0.02844rem;
 `
 
-export function PostCard() {
+export function PostCard(props) {
+    
     return (
         <>
         <Container>
             <PostWrapper>
+                <ImageWrapper src={props.imgUrl}/>
             </PostWrapper>
             <TitleWrapper>
-                Model 1
+                {props.title}
             </TitleWrapper>
         </Container>
         </>
