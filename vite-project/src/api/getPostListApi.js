df6a069a798973b7b225a9a4fb879af26c8a66d2
chc216 <mine3386@naver.com>
2023-11-14 23:11:31 +0900

    이외의 수정 코드 추가

---
+++ vite-project/src/api/getPostListApi.js
@@ -0,0 +1,14 @@
+import { PostModel, datas } from '../model';
+import {instance} from './instance';
+
+
+function getPostListApi(userId) {
+    // const jsonList = await instance.get('/getpostlist');
+    // const dataList = JSON.parse(jsonList);
+    const dataList = datas;
+    
+    const postList = dataList.map(data => new PostModel(data));
+    return postList;
+}
+
+export {getPostListApi};
