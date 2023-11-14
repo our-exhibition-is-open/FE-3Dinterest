83151b1dc0b25a70c77162b18b2e4c878a07e189
chc216 <mine3386@naver.com>
2023-11-14 23:05:33 +0900

    mainpage 수정

--- vite-project/src/page/mainPage.jsx
+++ vite-project/src/page/mainPage.jsx
@@ -2,22 +2,27 @@ import React from 'react';
 import { styled } from 'styled-components';
 
 import { NavigationBar } from '../component/common/NavigationBar.jsx';
-import { MasonryList } from '../component/mainpage/MasonryList.jsx'
-import { PostCard2 } from '../component/mainpage/PostCardMain.jsx';
+import { PostList } from '../component/mainpage/PostList.jsx'
+import { PostCardMain } from '../component/mainpage/PostCardMain.jsx';
 import { PostModel, datas } from '../model/PostModel.js';
+import {WelcomeHeader} from '../component/mainpage/WelcomeComponent';
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
+    align-items: center;
+    
 `
 
 export default function MainPage() {
-     //나중에 api에서 받아올 데이터 모델로 대체 예정, 여기서 필요한 데이터는 url과 타이틀, 그리고 게시글 번호
-     
+     // TODO: 나중에 api에서 받아올 데이터 모델로 대체 예정, 여기서 필요한 데이터는 url과 타이틀, 그리고 게시글 번호
+    const userName = "UserName";
+    const modelCount = 26;
     return(
         <Container>
             <NavigationBar />
-            <MasonryList />
+            <WelcomeHeader userName={userName} modelCount={modelCount}/>
+            <PostList />
         </Container>
     );
 }
