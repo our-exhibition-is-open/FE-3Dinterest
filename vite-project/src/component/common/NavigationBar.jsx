d0671a584060e7cc3c5dd2b11db964f263820661
chc216 <mine3386@naver.com>
2023-11-14 23:05:15 +0900

    Nav 디자인 blur 처리

--- vite-project/src/component/common/NavigationBar.jsx
+++ vite-project/src/component/common/NavigationBar.jsx
@@ -4,14 +4,21 @@ import { styled } from 'styled-components';
 import { ButtonComponent } from "./ButtonComponent";
 
 const Header = styled.header`
+        z-index: 10;
+        position: fixed;
+        top: 0;
+        /* width: 100% */
+        left: 0;
+        right: 0;
         display: flex;
         height: 30px;
         padding: 20px 20px 9px;
         justify-content: flex-end;
         align-items: flex-start;
         gap: 10px;
-    
-        background-color: #ffffff;
+
+        background:#ffffffea; backdrop-filter: blur(5px);
+
         box-shadow: 0px 0px 10px 0.1px rgba(0, 0, 0, 0.25);
 `;
 
