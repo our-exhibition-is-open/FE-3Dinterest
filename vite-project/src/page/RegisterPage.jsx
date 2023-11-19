473e06608a4ae69e0090554d99464b3ce4d63d7e
chc216 <mine3386@naver.com>
2023-11-19 20:22:31 +0900

    회원가입 페이지 구현

--- vite-project/src/page/RegisterPage.jsx
+++ vite-project/src/page/RegisterPage.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { InputComponent } from "../component/common/InputComponent";
+import { InputComponent2 } from "../component/common/InputComponent2";
 
 const BodyContainer = styled.div`
     display: flex;
@@ -16,15 +17,59 @@ const ContentsContainer = styled.div`
     gap: 3rem;
 `
 
+const PasswordContainer = styled.div`
+    gap: 1rem;
+    display: flex;
+    flex-direction: column;
+`
+
 export function RegisterPage() {
+    const [submitStatus, setsubmitStatus] = useState(false);
+    const [isIdProper, setIsIdProper] = useState(false);
+
+    const [value, setValue] = useState({id: "", pw: "", pwValid: "", email: ""});
+    const [properCount, setProperCount] = useState(0);
+    
+
+    function onChangeValue(value) {
+        setValue(value);
+    }
+    function onChangeProper() {
+
+    }
+
     return (
         <>
         <BodyContainer>
             <ContentsContainer>
-                <InputComponent inputType="id" checkingExp={/^[a-zA-z0-9]{4,12}$/} checkingFunc = {(v) => {return 1;}}/>
-                <InputComponent inputType="password" checkingExp={/^(?=.*[a-zA-Z])(?=.*[!@#$%^*+=-])(?=.*[0-9]).{8,25}$/} checkingFunc = {(v) => {return 1;}}/>
+                <form> 
+                    <InputComponent2 inputType="text" type="id" onChangeValue={onChangeValue}  value={value} placeholder="id를 입력하세요"/>
+                    <InputComponent2 inputType="password" type="pw" onChangeValue={onChangeValue}  value={value} placeholder="password를 입력하세요"/>
+                    <InputComponent2 inputType="password" type="pwValid" onChangeValue={onChangeValue}  value={value} placeholder="password 재확인"/>
+                    <InputComponent inputType="text" type="email" onChangeValue={onChangeValue}  value={value} placeholder="email를 입력하세요"/>
+                </form>
             </ContentsContainer>
         </BodyContainer>
         </>
     )
+}
+
+
+
+const handlingOnChangeValue2 = (e, checkingExp) => {
+    const currentValue = e.target.value;
+    setValue(currentValue);
+
+
+
+    if(!checkingExp.test(currentValue))
+    {
+        setMessage("다시 입력하세요");
+        setIsProper(false);
+    }
+    else
+    {
+        setMessage("굿");
+        setIsProper(true);
+    }
 }
