4bbeafe0577faabbe1c0822cea03d832c7f61e0b
chc216 <mine3386@naver.com>
2023-11-21 02:58:21 +0900

    폼 컴포넌트 수정

--- vite-project/src/component/common/RegisterFormComponent.jsx
+++ vite-project/src/component/common/RegisterFormComponent.jsx
@@ -1,3 +1,41 @@
 export function RegisterFormComponent() {
-    
+    return (
+        <>
+            <ContentsContainer>
+                    <InputComponent 
+                        inputType="text"
+                        type="id"
+                        onChangeValue={onChangeValue}
+                        value={value} 
+                        properCount={properCount} 
+                        placeholder="id"
+                    />
+                    <InputComponent 
+                        inputType="password" 
+                        type="pw" 
+                        onChangeValue={onChangeValue}  
+                        value={value} 
+                        properCount={properCount} 
+                        placeholder="password"
+                    />
+                    <InputComponent 
+                        inputType="password" 
+                        type="pwValid" 
+                        onChangeValue={onChangeValue}
+                        value={value} 
+                        properCount={properCount} 
+                        placeholder="password confirm"
+                    />
+                    <InputComponent 
+                        inputType="text" 
+                        type="email" 
+                        onChangeValue={onChangeValue}
+                        value={value} 
+                        properCount={properCount} 
+                        placeholder="email"
+                    />
+                    {/* <Button id="registerButton" formAction="">Sign up</Button> */}
+                    </ContentsContainer>
+        </>
+    )
 }
