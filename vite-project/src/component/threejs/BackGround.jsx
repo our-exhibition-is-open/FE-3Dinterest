27fcc6a1f7e58b37ba38f6c2021eca25d302ad66
chc216 <mine3386@naver.com>
2023-11-20 14:24:58 +0900

    mouse pointer interact

--- vite-project/src/component/threejs/BackGround.jsx
+++ vite-project/src/component/threejs/BackGround.jsx
@@ -37,22 +37,34 @@ function BackGround(props) {
         </Environment>
       </Canvas>
     )
-  }
-  
-  function Connector({ position, children, vec = new THREE.Vector3(), scale, r = THREE.MathUtils.randFloatSpread, accent, ...props }) {
-    const api = useRef()
-    const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
-    useFrame((state, delta) => {
-      delta = Math.min(0.1, delta)
-      api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
-    })
-    return (
-      <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
-        <CuboidCollider args={[0.38, 1.27, 0.38]} />
-        <CuboidCollider args={[1.27, 0.38, 0.38]} />
-        <CuboidCollider args={[0.38, 0.38, 1.27]} />
-        {children ? children : <Model {...props} />}
-        {accent && <pointLight intensity={4} distance={2.5} color={props.color} />}
-      </RigidBody>
-    )
-  }
+}
+
+function Connector({ position, children, vec = new THREE.Vector3(), scale, r = THREE.MathUtils.randFloatSpread, accent, ...props }) {
+const api = useRef()
+const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+useFrame((state, delta) => {
+    delta = Math.min(0.1, delta)
+    api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
+})
+return (
+    <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
+    <CuboidCollider args={[0.38, 1.27, 0.38]} />
+    <CuboidCollider args={[1.27, 0.38, 0.38]} />
+    <CuboidCollider args={[0.38, 0.38, 1.27]} />
+    {children ? children : <Model {...props} />}
+    {accent && <pointLight intensity={4} distance={2.5} color={props.color} />}
+    </RigidBody>
+)
+}
+
+function Pointer({ vec = new THREE.Vector3() }) {
+const ref = useRef()
+useFrame(({ mouse, viewport }) => {
+    ref.current?.setNextKinematicTranslation(vec.set((mouse.x * viewport.width) / 2, (mouse.y * viewport.height) / 2, 0))
+})
+return (
+    <RigidBody position={[0, 0, 0]} type="kinematicPosition" colliders={false} ref={ref}>
+    <BallCollider args={[1]} />
+    </RigidBody>
+)
+}
