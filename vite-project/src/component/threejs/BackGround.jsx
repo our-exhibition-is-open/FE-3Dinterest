148c97a4e699acba586c7104afdbf0c137389830
chc216 <mine3386@naver.com>
2023-11-20 14:23:53 +0900

    connector component

--- vite-project/src/component/threejs/BackGround.jsx
+++ vite-project/src/component/threejs/BackGround.jsx
@@ -39,3 +39,20 @@ function BackGround(props) {
     )
   }
   
+  function Connector({ position, children, vec = new THREE.Vector3(), scale, r = THREE.MathUtils.randFloatSpread, accent, ...props }) {
+    const api = useRef()
+    const pos = useMemo(() => position || [r(10), r(10), r(10)], [])
+    useFrame((state, delta) => {
+      delta = Math.min(0.1, delta)
+      api.current?.applyImpulse(vec.copy(api.current.translation()).negate().multiplyScalar(0.2))
+    })
+    return (
+      <RigidBody linearDamping={4} angularDamping={1} friction={0.1} position={pos} ref={api} colliders={false}>
+        <CuboidCollider args={[0.38, 1.27, 0.38]} />
+        <CuboidCollider args={[1.27, 0.38, 0.38]} />
+        <CuboidCollider args={[0.38, 0.38, 1.27]} />
+        {children ? children : <Model {...props} />}
+        {accent && <pointLight intensity={4} distance={2.5} color={props.color} />}
+      </RigidBody>
+    )
+  }
