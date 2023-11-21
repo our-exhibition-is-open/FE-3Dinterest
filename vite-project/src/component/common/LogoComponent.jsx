ce2ddc7943c06f8c43c13019d385e7fcd2a82c02
chc216 <mine3386@naver.com>
2023-11-21 22:56:56 +0900

    logo 추가

---
+++ vite-project/src/component/common/LogoComponent.jsx
@@ -0,0 +1,26 @@
+import styled from "styled-components";
+
+export const LogoContainer = styled.div`
+    cursor: pointer;
+    z-index: 1;
+    display: flex;
+    
+    align-items: center;
+    text-align: center;
+
+    z-index: 1;
+    margin-top: 10%;
+    margin-left: 5%;
+    width: 29rem;
+    /* background: #CF0000; */
+    background: linear-gradient(to right, #CF0000 14%, #0000FF 77%, #000000 95%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+
+    font-family: "San Francisco";
+    font-size: 5rem;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 0.05469rem;
+`
