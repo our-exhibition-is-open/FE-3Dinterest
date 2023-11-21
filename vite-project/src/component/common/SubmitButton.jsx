14254ff8b9bbbab8a31115d94027b6fbf6f36ed8
chc216 <mine3386@naver.com>
2023-11-21 22:58:42 +0900

    제출버튼 디자인 수정

--- vite-project/src/component/common/SubmitButton.jsx
+++ vite-project/src/component/common/SubmitButton.jsx
@@ -5,9 +5,14 @@ import styled, { keyframes } from "styled-components";
 export const AvailableButton = styled.button`
     height: 2rem;
     width: 100%;
-    background:#ffffff59;
-    backdrop-filter: blur(10px);
+    background:#ff0000;
+    backdrop-filter: blur(20px);
     border-radius: 0.4rem;
+
+    border: 0;
+    box-shadow: 0 0 0px #ff0000, 0 0 0px #ff2525c7, 0 0 0px #ff2222, 0 0 0px #ff1919,
+    0 0 5px #ff2020, 0 0 7px #ff3232;
+    /* background-color: transparent; */
 `
 
 const Image = styled.img`
@@ -18,11 +23,24 @@ export const NotAvailableButton = styled.button`
     z-index: 10;
     height: 2rem;
     width: 100%;
-    background:#ffffff59;
-    backdrop-filter: blur(10px);
+    background:#ff0000e6;
+    backdrop-filter: blur(50px);
     border-radius: 0.4rem;
+    border: 0;
+    /* background-color: transparent; */
+
+    font-family: "San Francisco";
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+    letter-spacing: 0.05469rem;
+    color: #ffffff;
+
     transform: translateX(${(props) => props.position}rem);
     transition: transform 0.2s ease;
+
+    box-shadow: 0 0 0px #ff0000, 0 0 0px #ff2525c7, 0 0 0px #ff2222, 0 0 0px #ff1919,
+    0 0 5px #ff2020, 0 0 7px #ff3232;
 `
 
 export function SubmitButton({buttonState}) {
