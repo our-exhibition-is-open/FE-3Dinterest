1c7378b82bfa95b09b6d64966c5c92b6adecb951
chc216 <mine3386@naver.com>
2023-11-26 19:20:41 +0900

    local storage 사용 연습

--- vite-project/src/page/mainPage.jsx
+++ vite-project/src/page/mainPage.jsx
@@ -4,18 +4,21 @@ import { styled } from 'styled-components';
 import { NavigationBar } from '../component/nav/NavigationBar.jsx';
 import { PostList } from '../component/mainpage/PostList.jsx'
 import {WelcomeHeader} from '../component/mainpage/WelcomeComponent';
+import { useUserStore } from '../model/userStore.js';
 
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     align-items: center;
-    
 `
 
 export default function MainPage() {
      // TODO: 나중에 api에서 받아올 데이터 모델로 대체 예정, 여기서 필요한 데이터는 url과 타이틀, 그리고 게시글 번호
-    const userName = "UserName";
+    
+
+    const userName = localStorage.getItem("userId");
+     
     const modelCount = 26;
     return(
         <Container>
