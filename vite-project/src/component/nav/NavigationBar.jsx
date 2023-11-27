6452e8de69d44d9fc02ad9159aa4a8947cafb51e
chc216 <mine3386@naver.com>
2023-11-28 03:23:39 +0900

    네비게이션 바 로고 추가

--- vite-project/src/component/nav/NavigationBar.jsx
+++ vite-project/src/component/nav/NavigationBar.jsx
@@ -5,6 +5,7 @@ import { styled } from "styled-components";
 import { Button, ButtonComponent } from "./ButtonComponent";
 import { Link } from "react-router-dom";
 import { useUserStore } from "../../model/userStore";
+import LogoComponent from "../common/LogoComponent";
 
 const Header = styled.header`
   z-index: 10;
@@ -15,7 +16,7 @@ const Header = styled.header`
   right: 0;
   display: flex;
   height: 30px;
-  padding: 20px 20px 9px;
+  padding: 20px 35px 9px;
   justify-content: flex-end;
   align-items: flex-start;
   gap: 10px;
@@ -26,16 +27,35 @@ const Header = styled.header`
   box-shadow: 0px 0px 10px 0.1px rgba(0, 0, 0, 0.376);
 `;
 
-export function NavigationBar(props) {
+const LogoContainer = styled.div`
+  background: linear-gradient(to right, #cf0000 14%, #0000ff 77%, #000000 95%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
 
+  margin-right: 70%;
+  font-size: 17px;
+  font-family: "SF-Pro-Rounded-Regular";
+  font-style: normal;
+  font-weight: 900;
+  line-height: normal;
+`;
+
+export function NavigationBar(props) {
   return (
     <>
       <Header>
+        {props.isLogged && (
+          <LogoContainer>
+            <Link to="/" style={{ color: "black", textDecoration: "none" }}>
+              3Dinterest
+            </Link>
+          </LogoContainer>
+        )}
         <div>
           {props.isLogged ? (
             <Button onClick={props.handleLogOut}>로그아웃</Button>
           ) : (
-            <ButtonComponent buttonName="로그인" route="/login" />
+            <ButtonComponent buttonName="Login" route="/login" />
           )}
         </div>
         <Button>
@@ -43,11 +63,14 @@ export function NavigationBar(props) {
             to="/register"
             style={{ color: "black", textDecoration: "none" }}
           >
-            회원가입
+            Sign Up
           </Link>
         </Button>
         <div>
-          <ButtonComponent buttonName="업로드" route="/upload" />
+          <ButtonComponent buttonName="Upload" route="/upload" />
+        </div>
+        <div>
+          <ButtonComponent buttonName="Contribute" route="/Contribute" />
         </div>
       </Header>
     </>
