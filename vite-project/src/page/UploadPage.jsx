5edf14d9f2aef4858a05334bbb50dd115525822b
chc216 <mine3386@naver.com>
2023-11-28 15:36:25 +0900

    업로드 페이지 개발

--- vite-project/src/page/UploadPage.jsx
+++ vite-project/src/page/UploadPage.jsx
@@ -1,6 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { NavigationBar } from "../component/nav/NavigationBar";
+import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
+import { FileComponent } from "../component/upload-page/FileComponent";
+import { UploadComponent } from "../component/upload-page/UploadComponent";
+
+const UploadContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  gap: 4rem;
+`;
+
 
 export default function UploadPage() {
-    
-    return ;
+  const navigate = useNavigate();
+  const [isLogged, setIsLogged] = useState(
+    sessionStorage.getItem("isLoggedIn") == "true" ? true : false
+  );
+  
+
+  useEffect(() => {
+    if (isLogged == false) {
+      alert("로그인 먼저 해주세요.");
+      navigate("/");
+    }
+  }, []);
+
+  function handleLogOut() {
+    setIsLogged(false);
+    // logoutUser();
+    alert("로그아웃되었습니다.");
+    sessionStorage.setItem("isLoggedIn", false);
+    sessionStorage.setItem("userId", null);
+    navigate("/");
+  }
+
+  return (
+    <>
+      <NavigationBar isLogged={isLogged} handleLogOut={handleLogOut} />
+      <UploadContainer>
+        <FileComponent/>
+        <UploadComponent/>
+      </UploadContainer>
+    </>
+  );
 }
