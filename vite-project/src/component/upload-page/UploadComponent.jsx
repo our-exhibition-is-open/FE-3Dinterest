62dfe7d810e46bcefb4a7b8003d6411a896eb47d
chc216 <mine3386@naver.com>
2023-11-28 15:36:43 +0900

    업로드 컴포넌트 개발

---
+++ vite-project/src/component/upload-page/UploadComponent.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const InputContainer = styled.div`
+  width: 35rem;
+  height: 45rem;
+  margin-top: 7%;
+
+  background-color: red;
+`;
+
+export function UploadComponent() {
+    return (
+      <InputContainer></InputContainer>
+    );
+}
