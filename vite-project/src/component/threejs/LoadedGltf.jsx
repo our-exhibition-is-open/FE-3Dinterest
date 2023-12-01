a769a1903c38cfc8d466d7c891d49d18172b248e
chc216 <mine3386@naver.com>
2023-12-02 00:39:08 +0900

    gltf로더 추가

---
+++ vite-project/src/component/threejs/LoadedGltf.jsx
@@ -0,0 +1,24 @@
+import React, { useRef, useState, useEffect, Suspense } from "react";
+import { useLoader, useThree } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Rhino3dmLoader } from "three/examples/jsm/loaders/3DMLoader";
+
+export function LoadedGltf({ file }) {
+  const { scene } = useThree();
+  const fileReader = new FileReader();
+
+  fileReader.onload = (event) => {
+    console.log(1);
+    const contents = event.target.result;
+    const loader = new GLTFLoader();
+    loader.parse(contents, "", (gltf) => {
+      console.log(2);
+      const object = gltf.scene;
+      object.castShadow = true;
+      object.receiveShadow = true;
+      console.log(object);
+      scene.add(object);
+    });
+  };
+  fileReader.readAsArrayBuffer(file);
+}
