656be863483c24b6d9da91a5d4096ce31b507f4a
chc216 <mine3386@naver.com>
2023-12-02 00:37:52 +0900

    제한 모드 해제

--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -16,8 +16,8 @@ async function enableMocking() {
 
 enableMocking().then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
+    // <React.StrictMode>
       <App />
-    </React.StrictMode>
+    // </React.StrictMode>
   );
 });
