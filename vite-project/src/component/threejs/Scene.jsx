c5d9ef31869dfb325569b7f27ab3a872ec89f52f
chc216 <mine3386@naver.com>
2023-12-02 01:26:05 +0900

    thumbnail컴포넌트 적용

--- vite-project/src/component/threejs/Scene.jsx
+++ vite-project/src/component/threejs/Scene.jsx
@@ -6,11 +6,13 @@ import { Rhino3dmLoader } from "three/examples/jsm/loaders/3DMLoader";
 import { LoadedGltf } from "./LoadedGltf";
 import { Loaded3Dm } from "./Loaded3Dm";
 import { fileNameParser } from "../../util/upload/fileNameParser";
+import { Thumbnail } from "./thumbnail";
 
 export function Scene(props) {
   const fileType = fileNameParser(props.file.name);
   const cameraControlRef = useRef(CameraControls, null);
 
+  
   return (
     <>
       <Canvas
@@ -23,6 +25,7 @@ export function Scene(props) {
           borderColor: "#9f9f9f",
         }}
         gl={{ preserveDrawingBuffer: true }}
+        id="uploadScene"
       >
         <color attach="background" args={["white"]} />
         <CameraControls ref={cameraControlRef} />
@@ -35,7 +38,7 @@ export function Scene(props) {
           angle={Math.PI / 3}
           position={[20, 36, 0]}
           castShadow={true}
-        />
+          />
         <SpotLight
           intensity={1000}
           penumbra={1}
@@ -43,13 +46,14 @@ export function Scene(props) {
           angle={Math.PI / 2.5}
           position={[-20, 36, 0]}
           castShadow={true}
-        />
+          />
         <gridHelper args={[100, 100]} />
         {fileType == "glb" ? (
           <LoadedGltf file={props.file} />
-        ) : (
-          <Loaded3Dm file={props.file} />
-        )}
+          ) : (
+            <Loaded3Dm file={props.file} />
+            )}
+      <Thumbnail/>
       </Canvas>
     </>
   );
