fa33ddca77473a48b3d3e1c28b5a81969fbcd655
chc216 <mine3386@naver.com>
2023-12-02 00:38:18 +0900

    Scene컴포넌트 모듈화를 위해 수정

--- vite-project/src/component/threejs/Scene.jsx
+++ vite-project/src/component/threejs/Scene.jsx
@@ -1,20 +1,56 @@
-import React, { useRef, useState, useEffect, Suspense } from 'react';
-import { Canvas, useFrame, useLoader } from '@react-three/fiber';
-import { CameraControls, SpotLight } from '@react-three/drei';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
-
+import React, { useRef, useState, useEffect, Suspense } from "react";
+import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { CameraControls, SpotLight } from "@react-three/drei";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { Rhino3dmLoader } from "three/examples/jsm/loaders/3DMLoader";
+import { LoadedGltf } from "./LoadedGltf";
+import { Loaded3Dm } from "./Loaded3Dm";
+import { fileNameParser } from "../../util/upload/fileNameParser";
 
 export function Scene(props) {
-  const gltf = useLoader(GLTFLoader, "src/resource/gltf/scene.gltf");
+  const fileType = fileNameParser(props.file.name);
   const cameraControlRef = useRef(CameraControls, null);
 
   return (
-    <Canvas camera={{position: [23,10,25]}}shadows style={{ borderRadius: 25 }}>
-        <color attach="background" args={["black"]}/>
-        <CameraControls ref={cameraControlRef}/>
-        <directionalLight intensity={0.8} position={[3,3,1]}/>
-        <SpotLight intensity={1000} penumbra={0.8} distance={100} angle={Math.PI/3} position={[0,40,0]} castShadow={true}/>
-          <primitive object={gltf.scene} position={[0,-1,0]} scale={[1,1,1]} castShadow={true}/>
-    </Canvas>
-  )
+    <>
+      <Canvas
+        camera={{ position: [23, 10, 25] }}
+        shadows
+        style={{
+          borderRadius: 25,
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "#9f9f9f",
+        }}
+        gl={{ preserveDrawingBuffer: true }}
+      >
+        <color attach="background" args={["white"]} />
+        <CameraControls ref={cameraControlRef} />
+        <directionalLight intensity={0.2} position={[0, 20, 30]} />
+        <directionalLight intensity={0.2} position={[0, 20, -30]} />
+        <SpotLight
+          intensity={1000}
+          penumbra={1}
+          distance={100}
+          angle={Math.PI / 3}
+          position={[20, 36, 0]}
+          castShadow={true}
+        />
+        <SpotLight
+          intensity={1000}
+          penumbra={1}
+          distance={100}
+          angle={Math.PI / 2.5}
+          position={[-20, 36, 0]}
+          castShadow={true}
+        />
+        <gridHelper args={[100, 100]} />
+        {fileType == "glb" ? (
+          <LoadedGltf file={props.file} />
+        ) : (
+          <Loaded3Dm file={props.file} />
+        )}
+      </Canvas>
+    </>
+  );
 }
