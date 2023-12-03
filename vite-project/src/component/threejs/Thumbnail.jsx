6a15ed74ce64c5ca2e11a7e3a73856ed122b28cb
chc216 <mine3386@naver.com>
2023-12-03 21:18:22 +0900

    썸네일 이미지 전역변수에 적용

--- vite-project/src/component/threejs/Thumbnail.jsx
+++ vite-project/src/component/threejs/Thumbnail.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useThree } from "@react-three/fiber";
 import { useUploadImageStore } from "../../model/useUploadImageStore";
+import { useUploadStore } from "../../model";
 
 export function Thumbnail() {
-    const { setImage } = useUploadImageStore();
+    const {setThumbnailImage} = useUploadStore();
     const {gl} = useThree();
     useEffect(() => {
         const image = gl.domElement.toDataURL("image/jpeg");
-        setImage(image);
+        setThumbnailImage(image);
     }, [])
 }
