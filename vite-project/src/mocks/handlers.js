7d94b1aa090ae94d8df6c586753b7d348f7df203
chc216 <mine3386@naver.com>
2023-12-04 16:41:20 +0900

    로그인 api msw 개발

--- vite-project/src/mocks/handlers.js
+++ vite-project/src/mocks/handlers.js
@@ -1,5 +1,6 @@
 import { http, HttpResponse } from "msw";
 import posts from "./dummyPost.json";
+import { checkingUser } from "../util/LoginUtil/checkingUser";
 
 export const handlers = [
   http.get("/", () => {
@@ -9,9 +10,17 @@ export const handlers = [
   http.get("/contribute", () => {
     return HttpResponse.json(posts);
   }),
-  
-  http.post("/api/posts/:postId/like", ({params}) => {
+
+  http.post("/api/posts/:postId/like", ({ params }) => {
     console.log("msw : " + params.postId);
-    return new HttpResponse(null, {status: 200})
-  })
+    return new HttpResponse(null, { status: 200 });
+  }),
+  http.post("/login", ({ params }) => {
+    console.log("msw : " + params.userId + "pw : " + params.password);
+    const enteredId = params.userId;
+    const enteredPw = params.password;
+    if (checkingUser(enteredId, enteredPw))
+      return new HttpResponse(null, { status: 200 });
+    else return new HttpResponse(null, { status: 400 });
+  }),
 ];
