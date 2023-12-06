a790cc25c0d8454d251be0e2ab7ae1f23725ec8e
chc216 <mine3386@naver.com>
2023-12-07 01:41:02 +0900

    모달 컴포넌트 변경사항 적용

--- vite-project/src/component/mainpage/modalComponent/PostModal.jsx
+++ vite-project/src/component/mainpage/modalComponent/PostModal.jsx
@@ -1,13 +1,17 @@
 import React, { Suspense, useEffect } from "react";
 import { useState } from "react";
 import styled from "styled-components";
+import MoonLoader from "react-spinners/MoonLoader";
+import HashLoader from "react-spinners/HashLoader";
 import { getPostListApi } from "../../../api/getPostListApi";
 
-import { PostModel } from "../../../model";
+import { PostModel, useLoadedModel } from "../../../model";
 import { ContributePost } from "./ContributePost";
-import { PostScene } from "../../threejsComponent/PostScene";
+import { UploadScene } from "../../threejsComponent/UploadScene";
 import { getContributeListApi } from "../../../api/getContributeListApi";
 import { LikeComponent } from "../../common/LikeComponent";
+import { getModelFileApi } from "../../../test/getModelFileApi";
+import { LoadingComponent } from "../../common/LoadingComponent";
 
 export const ModalBackground = styled.div`
   display: flex;
@@ -32,7 +36,7 @@ const ModalContainer = styled.div`
   z-index: 1001;
   flex-direction: row;
   width: 55.6875rem;
-  height: 39rem;
+  height: 36rem;
   justify-self: center;
   justify-content: center;
   position: fixed;
@@ -40,9 +44,9 @@ const ModalContainer = styled.div`
   bottom: 0;
   margin-top: auto;
   margin-bottom: auto;
-  padding-top: 1.6rem;
+  padding-top: 1.4rem;
   padding-right: 6.7px;
-  border-radius: 1.5rem;
+  border-radius: 26px;
   background: #fff;
 `;
 
@@ -60,8 +64,14 @@ const PostCanvas = styled.div`
   width: 39.375rem;
   height: 28.3rem;
   flex-shrink: 0;
-  position: static;
   margin-left: 1.37rem;
+  display: flex;
+  align-items: center;
+
+  /* border-style: solid;
+  border-radius: 26px;
+  border-width: 2px;
+  border-color: "#9f9f9f"; */
 `;
 
 const Title = styled.div`
@@ -83,11 +93,13 @@ const RowContainer = styled.div`
   display: flex;
   flex-direction: row;
   width: 100%;
+  gap: 0.3rem;
+  margin-top: 1%;
 `;
 
 const LikeContainer = styled.div`
   margin-top: 2%;
-  margin-left: 17%;
+  /* margin-left: 17%; */
 `;
 const InfoContainer = styled.div`
   display: flex;
@@ -129,33 +141,93 @@ const ContributeContainer = styled.div`
   }
 `;
 
+const LoadingContainer = styled.div`
+  display: flex;
+  justify-items: center;
+  margin-left: 46%;
+`;
+
+const DownloadContainer = styled.div`
+  margin-top: 2.18%;
+  margin-left: 12%;
+`;
+
 export function PostModal(props) {
-  const [postState, setPostState] = useState(props.post);//현재 모달이 띄워야할 post객체를 상태로 저장한다.
-  const [dataList, setDataList] = useState([]);
+  const [postState, setPostState] = useState(props.post); //현재 모달이 띄워야할 post객체를 상태로 저장한다.
+  const [contributePostList, setContributePostList] = useState(null);
+  const { setFileUrl, setType, setFile, file, resetLoadedModel } = useLoadedModel();
 
   useEffect(() => {
-    getContributeListApi(postState.postId).then((response) => {
-      //FIXME: 데이터 형식 맞추기 
-      setDataList(response);
+    setType(postState.type);
+    setFile(null);
+    getModelFileApi(postState.modelUrl)
+    .then((response) => {
+      console.log("called");
+      setFile(response.data);
+    }); //postState변경시마다 모델의 url을 가지고 파일을 가져옴. blob형식으로 전역변수에 저장한다.
+    
+    getContributeListApi(postState.postId)
+    .then((response) => {
+      //FIXME: 데이터 형식 맞추기
+      const ResponseDataList = JSON.parse(response).map(
+        (data) => new PostModel(data)
+      );
+      setContributePostList(
+        ResponseDataList.map((data, index) => (
+          <div
+            onClick={() => {
+              handlePostClick(data);
+            }}
+            key={index}
+          >
+            <ContributePost post={data} />
+          </div>
+        ))
+      );
     });
+
     console.log("request" + postState.postId);
+
+    return () => {
+      resetLoadedModel();
+    }
   }, [postState]);
 
   function handlePostClick(post) {
     setPostState(post);
   }
 
+  function handleDownload() {
+    
+  }
+
+  
   return (
     <>
       <ModalContainer>
         <ModalPostContainer>
           <PostCanvas>
-            <Suspense>
-              <PostScene gltfUrl={postState.modelUrl} />
-            </Suspense>
+            {file ? (
+              <Suspense>
+                <UploadScene />
+              </Suspense>
+            ) : (
+              <LoadingContainer>
+                {/* <HashLoader color="#d63636" speedMultiplier={0.9}/> */}
+                <MoonLoader color="#ff0000" size={33} />
+              </LoadingContainer>
+            )}
           </PostCanvas>
           <RowContainer>
             <Title>{postState.title}</Title>
+            <DownloadContainer>
+              <a href={postState.modelUrl}>
+                <img
+                  src="/src/resource/image/icon_download.png"
+                  style={{ width: "28.31px", height: "26.8px" }}
+                />
+              </a>
+            </DownloadContainer>
             <LikeContainer>
               <LikeComponent
                 isLogged={props.isLogged}
@@ -165,23 +237,19 @@ export function PostModal(props) {
           </RowContainer>
           <InfoContainer>
             <UploadTimeWrapper>
-              upload time: {postState.uploadData}
+              upload time: {postState.uploadDate}
             </UploadTimeWrapper>
             <UserWrapper>User: {postState.userId}</UserWrapper>
           </InfoContainer>
         </ModalPostContainer>
         <ContributeContainer>
-          {dataList ?
-            dataList.map((data, index) => (
-              <div
-                onClick={() => {
-                  handlePostClick(new PostModel(data));
-                }}
-                key={index}
-              >
-                <ContributePost post={new PostModel(data)} />
-              </div>
-            )) : <div>2차 창작물이 없습니다.</div>}
+          {contributePostList ? (
+            contributePostList
+          ) : (
+            <UploadTimeWrapper style={{ marginTop: "60%" }}>
+              2차 창작물이 없습니다.
+            </UploadTimeWrapper>
+          )}
         </ContributeContainer>
       </ModalContainer>
     </>
