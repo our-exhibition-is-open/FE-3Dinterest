21626953a2442b96fd75cd88e4b783696765713a
chc216 <mine3386@naver.com>
2023-12-06 11:24:12 +0900

    main페이지에서 전체 게시글 조회 api를 실제 형식에 맞게 적용합니다.

--- vite-project/src/api/getPostListApi.js
+++ vite-project/src/api/getPostListApi.js
@@ -1,9 +1,9 @@
 import axiosInstance from './instance';
 
 async function getPostListApi(userId) {
-    const response = await axiosInstance.get('/');
-    console.log("good")
-    return await response.data;
+    const response = await axiosInstance.get('/postlist');
+    
+    return await response;
 }
 
 
