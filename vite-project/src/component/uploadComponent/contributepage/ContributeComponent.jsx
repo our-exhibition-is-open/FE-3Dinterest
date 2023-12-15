e00308e5c33969008335780276aac08f3fe835fd
chc216 <mine3386@naver.com>
2023-12-16 01:27:53 +0900

    컨트리뷰트 페이지 레이아웃을 수정한다

--- vite-project/src/component/uploadComponent/contributepage/ContributeComponent.jsx
+++ vite-project/src/component/uploadComponent/contributepage/ContributeComponent.jsx
@@ -1,27 +1,32 @@
 import React from "react";
 import styled from "styled-components";
+import { UploadInputComponent } from "../uploadpage/UploadInputComponent";
+import { UploadSubmitComponent } from "../uploadpage/UploadSubmitComponent";
+import { useUploadStore } from "../../../model";
+import { ContributeListComponent } from "./ContributeListComponent";
+
 
 const Container = styled.div`
-  z-index: 100;
   display: flex;
-  flex-direction: row;
-  justify-content: start;
-  visibility: hidden;
-
-  width: 100%;
-  height: 4rem;
-  background-color: #000000;
-
-  &::-webkit-scrollbar {
-    display: none;
-  }
+  flex-direction: column;
+  width: 35rem;
+  height: 100%;
+  padding-top: 15%; 
+  gap: 1rem;
 `;
+
+//FIXME: 컨트리뷰트 컴포넌트 추가할 예정
 export function ContributeComponent() {
+  
+  function handleContribute() {
+
+  }
+
   return (
-    <>
-      <Container>
-        
-      </Container>
-    </>
+    <Container>
+      <UploadInputComponent/>
+      <ContributeListComponent/>
+      <UploadSubmitComponent api={handleContribute}/>
+    </Container>
   );
 }
