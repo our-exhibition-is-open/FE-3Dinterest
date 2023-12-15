4097ce94eaeada534bddc39c86d71ff42447fc38
chc216 <mine3386@naver.com>
2023-12-16 04:06:21 +0900

    msw 서버 다운로드 히스토리 api 모킹

--- vite-project/src/mocks/handlers.js
+++ vite-project/src/mocks/handlers.js
@@ -11,6 +11,10 @@ export const handlers = [
     console.log(params);
     return HttpResponse.json(JSON.stringify(posts));
   }),
+  http.get("/contribute_list/:userId", ({params}) => {
+    console.log(params);
+    return HttpResponse.json(JSON.stringify(posts));
+  }),
 
   http.post("/api/posts/:postId/like", async ({ request, params }) => {
     const { postId } = params;
@@ -32,5 +36,10 @@ export const handlers = [
     const {post_id, user_id} = await request.json();
     console.log({post_id, user_id});
     return new HttpResponse(null, {status: 200});
-  })
+  }),
+  http.post('/upload', async ({request}) => {
+    console.log(request.formData());
+    console.log("msw mocking upload api success!");
+    return new HttpResponse(null, {status: 200});
+  }),
 ];
