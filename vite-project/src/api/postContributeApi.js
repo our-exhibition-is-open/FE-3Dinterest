a77a183fc3ed3afd7ac09655fb41741bffb92aa4
chc216 <mine3386@naver.com>
2023-12-16 04:05:12 +0900

    컨트리뷰트 api 개발

--- vite-project/src/api/postContributeApi.js
+++ vite-project/src/api/postContributeApi.js
@@ -1,9 +1,8 @@
 import axiosInstance from "./instance";
 
-export async function postContributeApi(contribute_data_model) {
+export async function postContributeApi(parentId) {
   const response = await axiosInstance.post(`/contribute_record`, {
-    post_id: contribute_data_model.postId,
-    parent_id: contribute_data_model.parentId,
+    parent_id: parentId,
   },{
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded'
